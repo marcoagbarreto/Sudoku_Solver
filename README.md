a0efd67332c2ea8f4d874c32f98a8652fdecf90b
Marco.Barreto <marco.agbarreto@gmail.com>
2023-01-16 23:24:03 -0600

    added solver

---
+++ README.md
@@ -0,0 +1,23 @@
+# spotify-ad-mute
+This is a program to solve sudokus.
+
+## Example
+
+1. Edit the board inside ```sudoku_solver.py```
+2. Run ```sudoku_solver.py```
+3. Profit.
+
+## Code Usage
+
+clone the repository (no installation required, source files are sufficient):
+        
+    https://github.com/marcoagbarreto/Sudoku_Solver.git
+
+dependencies:
+
+    None
+
+or [download and extract the zip](https://github.com/marcoagbarreto/Sudoku_Solver/archive/main.zip) into your project folder.
+
+## Known limitations:
+* Sudoku format should be in an array form.
---
+++ sudoku_solver.py
@@ -0,0 +1,78 @@
+"""
+Title: Sudoku_Solver
+Description: This is a program to solve sudokus.
+Author: Marco A. Barreto - marcoagbarreto
+Version: 16-Jan-2023
+"""
+
+
+def solve(board):
+    find = find_empty(board)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1,10):
+        if valid(board, i, (row, col)):
+            board[row][col] = i
+
+            if solve(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
+
+
+def valid(board, num, pos):
+    # Check row
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Check column
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x * 3, box_x*3 + 3):
+            if board[i][j] == num and (i,j) != pos:
+                return False
+
+    return True
+
+
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)  # row, col
+    return None
+
+
+def main():
+    # Test the solver
+    board = [[5,3,0,0,7,0,0,0,0],
+             [6,0,0,1,9,5,0,0,0],
+             [0,9,8,0,0,0,0,6,0],
+             [8,0,0,0,6,0,0,0,3],
+             [4,0,0,8,0,3,0,0,1],
+             [7,0,0,0,2,0,0,0,6],
+             [0,6,0,0,0,0,2,8,0],
+             [0,0,0,4,1,9,0,0,5],
+             [0,0,0,0,8,0,0,7,9]]
+
+    if solve(board):
+        print(board)
+    else:
+        print("No solution exists")
+
+
+if __name__ == "__main__":
+    main()
